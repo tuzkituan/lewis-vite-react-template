7d553da5f24952523dcc9cf906e302d1098c96bc
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-09-15 17:02:04 +0700

    update homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,35 +1,47 @@
 import { useState } from 'react';
 
-import { Box, Button, Center, Flex, Image, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Image,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
 
 function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <Box w="100%" h="100vh" p={6}>
+    <Box w="100%" h="100vh" paddingInline={10} paddingBlock={6}>
       <Flex
         justifyContent="center"
         flexDir="column"
         alignItems="center"
         h="100%"
-        gap={4}
+        gap={6}
       >
         <Center>
           <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <Image src="/vite.svg" className="logo" alt="Vite logo" w="10vw" />
+            <Image src="/vite.svg" className="logo" alt="Vite logo" w="15vw" />
           </a>
           <a href="https://reactjs.org" target="_blank" rel="noreferrer">
             <Image
               src="./react.svg"
               className="logo react"
               alt="React logo"
-              w="10vw"
+              w="15vw"
             />
           </a>
         </Center>
-        <Text fontWeight="bold">
-          Vite + React + Typescript + Chakra UI + React Router v6 + Redux
-          Toolkit + Eslint + Axios
+        <Text
+          fontWeight="bold"
+          fontSize={24}
+          textAlign="center"
+          letterSpacing={2}
+        >
+          HELLO WORLD
         </Text>
         <Center>
           <Button
@@ -40,11 +52,11 @@ function Home() {
           </Button>
         </Center>
         <Center>
-          <Text fontSize={16}>
+          <Text fontSize={32}>
             Edit <code>src/App.tsx</code> and save to test HMR
           </Text>
         </Center>
-        <Text fontSize={12}>
+        <Text fontSize={16}>
           Click on the Vite and React logos to learn more
         </Text>
       </Flex>
