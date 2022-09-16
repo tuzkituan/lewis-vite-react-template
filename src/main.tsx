0f3cd191ebe4c1af5d8c0946ffb6857b090afae8
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-09-16 11:36:38 +0700

    add redux to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from 'store';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
 import theme from './theme';
+
+function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <MainApp />
+    </Provider>
+  );
+}
 
 function MainApp() {
   return (
@@ -19,6 +29,6 @@ function MainApp() {
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MainApp />
+    <AppWrapper />
   </React.StrictMode>
 );
